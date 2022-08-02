9e9921d831645634817fba4baf5853c7e87af067
ruslanlap <lapin@ucu.edu.ua>
2022-08-02 23:24:40 +0300

    with pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // загальне  підключення GULP
-const { src,dest,watch,series,parallel } = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
 const browsersync = require("browser-sync").create(); //запустити сервер
 const del = require("del"); // видалити  папку
 
@@ -10,60 +10,49 @@ const htmlmin = require("gulp-htmlmin"); // зжати  html
 const size = require("gulp-size"); // вихначення ваги файлів після зжимання
 const plumber = require("gulp-plumber"); // перевіряти помилки
 const notify = require("gulp-notify"); // вивести помилку
-// const rename = require("gulp-rename"); // перейменувати
+const pugs = require("gulp-pug"); //
 
-// робота з HTML
-const html = () => {
-	return src("./src/html/*.html") // маски шляхів
+// робота з PUG
+const pug = () => {
+	return src("./src/pug/*.pug") // маски шляхів
 		.pipe(plumber({ errorHandler: notify.onError() })) // вивести помилку
-		.pipe(gulpFileInclude()) //виконати ключення частин файлів в основний файл
 		.pipe(
-			size({
-				title: "до зжимання",
+			pugs({
+				pretty: true,
+				data: {
+					news: require("./data/news.json"),
+				},
 			}),
-		)
-		.pipe(
-			htmlmin({
-				collapseWhitespace: true,
-				removeComments: true,
-			}),
-		)
-		.pipe(
-			size({
-				title: "після зжимання",
-			}),
-		)
+		) //without minification
 		.pipe(dest("./public"))
 		.pipe(browsersync.stream());
 };
 
 //спостерігач
 const watcher = () => {
-	watch("./src/html/**/*.html", html);
+	watch("./src/pug/**/*.pug", pug);
 };
 
 // // чистити директорію
 const clears = () => {
 	return del("./public/");
-}
+};
 
 // server
 function server() {
-  browsersync.init({
-    server: {
-      baseDir: "./public",
-    },
-  });
+	browsersync.init({
+		server: {
+			baseDir: "./public",
+		},
+	});
 }
 
 // задачі
-exports.html = html;
+exports.pug = pug;
 // exports.css = css;
 // exports.js = js;
 exports.watch = watcher;
 exports.clears = clears;
 
 // серія
-exports.dev = series(clears,html,
-	parallel(watcher,server)
-)
+exports.dev = series(clears, pug, parallel(watcher, server));
